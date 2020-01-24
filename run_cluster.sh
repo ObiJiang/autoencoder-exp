4b8459816810ecf9387a195a788a3a9744513c4f
Yibo Jiang <jyb940413@gmail.com>
2020-01-23 20:15:35 -0500

    run in clusters

---
+++ run_cluster.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+inputdims=(16)
+nbFixedPoints=(5)
+nbLayers=(2)
+hiddenDims=(1000)
+Dirs=('test')
+
+for inputDim in "${inputdims[@]}"
+do
+
+for nbFixedPoint in "${nbFixedPoints[@]}"
+do
+
+for nbLayer in "${nbLayers[@]}"
+do
+
+for hiddenDim in "${hiddenDims[@]}"
+do
+
+for Dir in "${Dirs[@]}"
+do
+
+echo $inputDim, $nbFixedPoint, $nbLayer, $hiddenDim, $Dir, 0.1, 1, 10
+export inputDim nbFixedPoint nbLayer hiddenDim
+
+sbatch -o out_i${inputDim}_np${nbFixedPoint}_nl${nbLayer}_h${hiddenDim}.stdout.txt \
+-e err_i${inputDim}_np${nbFixedPoint}_nl${nbLayer}_h${hiddenDim}.stdout.txt \
+--job-name=auto_encoder_ri${inputDim}_np${nbFixedPoint}_nl${nbLayer}_h${hiddenDim} \
+run_script.sbatch
+
+sleep 1
+
+done
+done
+done
+done
+done
