1bf138959c732ad3bc6810beda4bbcd53a770d35
Yibo Jiang <jyb940413@gmail.com>
2020-01-28 20:12:11 -0500

    more experiments

--- run_cluster.sh
+++ run_cluster.sh
@@ -1,11 +1,54 @@
 #!/bin/bash
 
-# large activation test
-inputdims=(100)
-nbFixedPoints=(20 50 80)
+# 2-layer 40 point test
+inputdims=(16 32)
+nbFixedPoints=(40)
 nbLayers=(2)
-hiddenDims=(1000)
-Dirs=('/n/scratchlfs02/pehlevan_lab/yibo_autoencoder_eig_linear')
+hiddenDims=(1000 10000 100000 500000 1000000)
+Dirs=('/n/scratchlfs02/pehlevan_lab/yibo_autoencoder_eig_add_on')
+Acts=('sigmoid')
+
+for inputDim in "${inputdims[@]}"
+do
+
+for nbFixedPoint in "${nbFixedPoints[@]}"
+do
+
+for nbLayer in "${nbLayers[@]}"
+do
+
+for hiddenDim in "${hiddenDims[@]}"
+do
+
+for Dir in "${Dirs[@]}"
+do
+
+for Act in "${Acts[@]}"
+do
+
+echo $inputDim, $nbFixedPoint, $nbLayer, $hiddenDim, $Dir, $Act, 0.1, 1, 10
+export inputDim nbFixedPoint nbLayer hiddenDim Dir Act
+
+sbatch -o out_eig_i${inputDim}_np${nbFixedPoint}_nl${nbLayer}_h${hiddenDim}_a${Act}.stdout.txt \
+-e err_eig_i${inputDim}_np${nbFixedPoint}_nl${nbLayer}_h${hiddenDim}_a${Act}.stdout.txt \
+--job-name=auto_encoder_ri${inputDim}_np${nbFixedPoint}_nl${nbLayer}_h${hiddenDim}_a${Act} \
+run_script.sbatch
+
+sleep 1
+
+done
+done
+done
+done
+done
+done
+
+# 3-layer, single point test
+inputdims=(16, 32)
+nbFixedPoints=(1)
+nbLayers=(3, 4)
+hiddenDims=(100 1000 10000)
+Dirs=('/n/scratchlfs02/pehlevan_lab/yibo_autoencoder_eig_add_on')
 Acts=('sigmoid' 'tanh')
 
 for inputDim in "${inputdims[@]}"
@@ -43,16 +86,45 @@ done
 done
 done
 
-# # 3-layer
-# inputdims=(16)
-# nbFixedPoints=(1 5 20 100)
-# nbLayers=(3)
-# hiddenDims=(100 1000 10000)
-# Dirs=('/n/scratchlfs02/pehlevan_lab/yibo_autoencoder_eig')
+# large activation test for eigen spectrum
+inputdims=(16, 32)
+nbFixedPoints=(20)
+nbLayers=(2)
+hiddenDims=(1000, 10000, 100000)
+Dirs=('/n/scratchlfs02/pehlevan_lab/yibo_autoencoder_eig_add_on')
+Acts=('sigmoid' 'tanh' 'erf')
 
-# # 2-layer change input dims
-# inputdims=(8 32)
-# nbFixedPoints=(1 5 20 100)
-# nbLayers=(2)
-# hiddenDims=(1000 10000 100000 500000 1000000)
-# Dirs=('/n/scratchlfs02/pehlevan_lab/yibo_autoencoder_eig')
+for inputDim in "${inputdims[@]}"
+do
+
+for nbFixedPoint in "${nbFixedPoints[@]}"
+do
+
+for nbLayer in "${nbLayers[@]}"
+do
+
+for hiddenDim in "${hiddenDims[@]}"
+do
+
+for Dir in "${Dirs[@]}"
+do
+
+for Act in "${Acts[@]}"
+do
+
+echo $inputDim, $nbFixedPoint, $nbLayer, $hiddenDim, $Dir, $Act, 0.1, 1, 10
+export inputDim nbFixedPoint nbLayer hiddenDim Dir Act
+
+sbatch -o out_eig_i${inputDim}_np${nbFixedPoint}_nl${nbLayer}_h${hiddenDim}_a${Act}.stdout.txt \
+-e err_eig_i${inputDim}_np${nbFixedPoint}_nl${nbLayer}_h${hiddenDim}_a${Act}.stdout.txt \
+--job-name=auto_encoder_ri${inputDim}_np${nbFixedPoint}_nl${nbLayer}_h${hiddenDim}_a${Act} \
+run_script.sbatch
+
+sleep 1
+
+done
+done
+done
+done
+done
+done
